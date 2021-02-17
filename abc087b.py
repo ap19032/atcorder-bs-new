f49ee00d04dd3833cac3e60b52c822b5aa964b84
AP19032 ホッサイン シャクワット <ap19032@azec.ac.jp>
2021-02-17 14:56:52 +0900

    performance

--- abc087b.py
+++ abc087b.py
@@ -1,3 +1,4 @@
+
 a = int(input())
 b = int(input())
 c = int(input())
@@ -6,9 +7,10 @@ x = int(input())
 num_combination = 0
 for n500 in range(0,a+1):
     for n100 in range(0,b+1):
-        for n50 in range(0,c+1):
-            amount = n500*500+n100*100+n50*50
-            if x==amount:
-                num_combination +=1
+        n50 =(x-n500*500-n100*100)//50
+
+        if n50 <=c and n50>=0:
+            print(n500, n100,n50)
+            num_combination +=1
 
 print(num_combination)
